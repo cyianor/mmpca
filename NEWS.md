04c00019fa8c7e448222007e4fc610e781631b0b
Felix Held <felix.held@gmail.com>
2022-09-08 09:08:35 +0200

    Conform to CRAN expectations

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# mmpca 2.0.2 (2022-09-03)
+ 
+* Package has been cleaned up to be published on CRAN again
+* Removed all legacy C code and use Rcpp fully
--- tests/test.R
+++ tests/test.R
@@ -14,10 +14,12 @@ test_lambda_selection <- function() {
   X[[3]] <- v[[3]][, 1:2] %*% t(v[[4]][, 3:4])
   X[[4]] <- v[[1]][, c(1, 3)] %*% t(v[[5]][, 1:2])
   X[[1]][matrix(runif(8 * 8), 8, 8) < 0.2] <- NA
-  mmpca::mmpca(X, inds, 3, parallel = FALSE, trace = 3)
+  mmpca::mmpca(X, inds, 3, parallel = TRUE)
   # only checking for errors, not checking the result
   return(TRUE)
 }
-options(mc.cores = 2)
+opt <- options(mc.cores = 2)
 set.seed(1)
 test_lambda_selection()
+# Reset options
+options(opt)
